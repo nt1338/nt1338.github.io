d98fc011afaca683de448b7e551d11ebe9f3c43c
Nikolay Trefilov <Nikolay.Trefilov@iag.co.nz>
2021-07-07 16:33:11 +1200

    PWA

---
+++ pwa1/service-worker-pwa1.js
@@ -0,0 +1,11 @@
+﻿var CACHE_NAME = 'myPWA1-cache-v1';
+
+self.addEventListener('install', function (event) {
+});
+
+self.addEventListener('activate', function (event) {
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('fetch!!!');
+});
